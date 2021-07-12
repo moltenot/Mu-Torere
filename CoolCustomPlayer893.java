731c4172beb4beacd97e694600d3a302ba4cefe7
Daniel Blaikie <blada911@student.otago.ac.nz>
2021-07-12 17:43:27 +1200

    Created the custom player implementation [THIS WILL NOT COMPILE WHEN RUN AS I HAVE NOT ADDED ANY RETURN STATEMENTS TO THE GETMOVE METHOD]

---
+++ CoolCustomPlayer893.java
@@ -0,0 +1,29 @@
+package MuTorere;
+
+import MuTorere.Player;
+
+/**Our implementation of the Player abstract class.
+  * COSC326 Etude 1
+  * Daniel Blaikie
+  * Blake MacDade
+  * Sam Fern
+  * Theo Molteno
+  */
+  class CoolCustomPlayer893 : Player{
+    
+    /**Constructor - creates a new custom player in the same way the original Player class does.*/
+    public CoolCustomPlayer893(BoardReader boardReader, Board.Piece playerID){
+      super(boardReader, playerID);
+    }
+    
+    
+    /*
+     Need to implement this.
+     Return the index of the piece that you want to move.
+     If the result is not a valid move, you lose.
+     If there are no valid moves, just return something - don't leave us hanging!
+     */
+    public int getMove(){
+      
+    }
+  }
