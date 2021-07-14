5102fd206b3390b96e08f6aa9deca69e82e4a140
Daniel Blaikie <blada911@student.otago.ac.nz>
2021-07-14 17:00:41 +1200

    Implemented board rotation.

--- MuTorere/CustomPlayer.java
+++ MuTorere/CustomPlayer.java
@@ -9,13 +9,16 @@ package MuTorere;
   */
   class CustomPlayer extends Player{
     
-    //ARRAY of board pieces, array index 8 is always the centre peice.
-    private Board.Piece[] boardArray = new Board.Piece[9];
+    //ARRAY of board pieces, array index 8 is always the centre piece.
+    private Board.Piece[] boardArray;
+    private int numKawai;
 
 
     /**Constructor - creates a new custom player in the same way the original Player class does.*/
     public CustomPlayer(BoardReader boardReader, Board.Piece playerID){
       super(boardReader, playerID);
+      boardArray  = new Board.Piece[9];
+      numKawai = boardArray.length - 1;
     }
     
     
@@ -26,18 +29,32 @@ package MuTorere;
      If there are no valid moves, just return something - don't leave us hanging!
      */
     public int getMove(){
-        return 2147483647;
+      loadBoardArray();
+      return 2147483647;
     }
     
     private void rotateBoardArray(int numPlaces){
+      Board.Piece[] boardArrayClone = boardArray.clone();
 
-      for(int i = 0; i < boardArray.length - 1; i++){
+      for(int i = 0; i < numKawai; i++){
+        boardArray[i] = boardArrayClone[(i + numPlaces) % numKawai];
+      }
+    }
+
+    private void flipBoardArray(){
+      for(int i = 0; i < numKawai / 2; i++){
         
       }
     }
 
+    private void swapArrayPositions(int i, int j){
+      Board.Piece temp = boardArray[i];
+      boardArray[i] = boardArray[j];
+      boardArray[j] = temp;
+    }
+
     private void loadBoardArray(){
-        if(BoardReader != null){
+        if(boardReader != null){
           for(int i = 0; i < boardArray.length; i ++){
               boardArray[i] = boardReader.pieceAt(i); 
           }
